66aeceea32ffe5a50f60ed9f8fc126a870135b0f
muzzled2b <muzzled2b@gmail.com>
2022-12-09 15:33:56 +0000

    limiting forms sizes, adding comments and updated all header and footers to include titles and alt
    limiting forms sizes, adding comments and updated all header and footers to include titles and alt

--- script/contactUsScript.js
+++ script/contactUsScript.js
@@ -2,7 +2,7 @@ function myJsAboutUsFuntion(){
     var name = document.getElementById("nameField").value;
     var email = document.getElementById("emailField").value;
     let x = name + " thank you for your details. We will be in touch via "+email + " shortly.";
-    document.getElementById("demo").innerHTML = x;    //
-    document.getElementById("myForm").style.display= "none";   //hiding the form
+    document.getElementById("myReturn").innerHTML = x;    //
+    document.getElementById("contactUsForm").style.display= "none";   //hiding the form
     alert(x);
 }
