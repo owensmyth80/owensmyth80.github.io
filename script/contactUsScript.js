c92ec9901fefd9bdbd1f1329de39d41ce2cccbd7
muzzled2b <muzzled2b@gmail.com>
2022-12-10 14:53:44 +0000

    Update contactUsScript.js
    clean up code

--- script/contactUsScript.js
+++ script/contactUsScript.js
@@ -3,30 +3,18 @@ function myJsContactUsFuntion(){
     let textBox = document.getElementById("message").value;
     var email = document.getElementById("emailField").value;
     let x = name + " Thank you for your details. We will be in touch via "+email + " shortly.";
-//    var nameArray = [document.getElementById("nameField")];    this isn't needed as i can loop already to check for the illegal characters
     let count = 0;
-    //    nameArray = name;
-
+    
     for(i=0;i<textBox.length;i++){
 
         if (textBox[i] =="*" || textBox[i] == "(" || textBox[i] == ")" || textBox[i] == "{" || textBox[i] == "}" || textBox[i] == "=" || textBox[i] == "[" || textBox[i] == "]"){
-//            alert("bad entry");
             count++;
         }
-//        else{
-//            alert("OK Entry")
-//       }
-//        alert(count);
-
     } 
-
-//    alert(count);   //  checking that count keeps the cout out of the loop 
-
 
     if(count == 0){
         document.getElementById("myReturn").innerHTML = x;    //
         document.getElementById("contactUsForm").style.display= "none";   //hiding the form
-//        alert(x);
         return;
     }
     else{
@@ -34,16 +22,4 @@ function myJsContactUsFuntion(){
         return;
     }
 } 
-    /*       else{
-            document.getElementById("myReturn").innerHTML = x;    //
-            document.getElementById("contactUsForm").style.display= "none";   //hiding the form
-            alert(x);
-        }
-        }
-    }
-/*
-    let x = name + " thank you for your details. We will be in touch via "+email + " shortly.";
-    document.getElementById("myReturn").innerHTML = x;    //
-    document.getElementById("contactUsForm").style.display= "none";   //hiding the form
-    alert(x);
-*/
+
